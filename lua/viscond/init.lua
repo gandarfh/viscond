350cb2cb912bb0e4656277f4d163fcfa236fc378
gandarfh <jgdsantosferreira@gmail.com>
2022-06-19 13:19:39 -0300

    fix: linting

--- lua/viscond/init.lua
+++ lua/viscond/init.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_command("hi clear")
 if vim.fn.exists("syntax_on") then
-    vim.api.nvim_command("syntax reset")
+  vim.api.nvim_command("syntax reset")
 end
 vim.o.background = "dark"
 vim.o.termguicolors = true
@@ -18,9 +18,9 @@ local LSP = require("viscond.LSP")
 
 
 local skeletons = {
-    highlights, Treesitter, markdown, Whichkey, Git, LSP
+  highlights, Treesitter, markdown, Whichkey, Git, LSP
 }
 
 for _, skeleton in ipairs(skeletons) do
-    util.initialise(skeleton)
+  util.initialise(skeleton)
 end
